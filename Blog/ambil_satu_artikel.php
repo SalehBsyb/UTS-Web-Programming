c375cb7c7530dcd30a9dfd319fe79ed732dd93d4
SalehBsyb <salehbasyaeb15@gmail.com>
2026-04-28 23:43:15 +0700

    Delete Blog directory

--- Blog/ambil_satu_artikel.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'koneksi.php';
-
-$id = $_GET['id'];
-$stmt = $pdo->prepare("SELECT * FROM artikel WHERE id = ?");
-$stmt->execute([$id]);
-echo json_encode($stmt->fetch(PDO::FETCH_ASSOC));
-?>
